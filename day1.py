27f5a3f3416433b1516c7a119697236df54cdd8a
Jaya Vellayan <jlv7cr@virginia.edu>
2021-06-25 11:01:20 -0400

    juypter

--- day1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-demo = pd.read_csv('archive_2/demographic.csv')
-diet = pd.read_csv('archive_2/diet.csv')
-exam = pd.read_csv('archive_2/examination.csv')
-labs = pd.read_csv('archive_2/labs.csv')
-#meds = pd.read_csv('archive_2/medications.csv')
-questions = pd.read_csv('archive_2/questionnaire.csv')
-
