77e7b2b4d30c096bee87d1dc2e92c9079301b1d4
Colin O'Brien <cmobrien123@gmail.com>
2021-07-02 14:45:59 -0400

    Delete cleaning_excel_script.py

--- cleaning_excel_script.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import os
-import sys
-
-my_path = os.chdir('/Users/colinobrien/Desktop/Repo/DS_5100/ds5100_prog_for_ds_jv')
-COVID_data = pd.read_excel('MortalityDataWHR2021C2.xlsx')
-print(COVID_data)
